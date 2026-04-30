6af827a544f459679e9529d1d7bab44e99adfa67
Gustavo Fernandes <gustavof023044@gmail.com>
2026-04-30 20:00:01 -0300

    Add files via upload

---
+++ MeuCurriculo/Models/Experiencia.cs
@@ -0,0 +1,10 @@
+namespace MeuCurriculo.Models;
+
+public class Experiencia
+{
+    public string Empresa { get; set; } = "Metalfisa";
+    public string Cargo { get; set; } = "";
+    public string Inicio { get; set; } = "";
+    public string Fim { get; set; } = "Atual";
+    public string Descricao { get; set; } = "";
+}
